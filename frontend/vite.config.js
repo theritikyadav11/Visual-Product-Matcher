1301b2f76dd0f35af4f9f72a398b2803f96d29c1
theritikyadav11 <arvindkumar426390@gmail.com>
2025-10-15 02:35:29 +0530

    Updated backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://visual-product-matcher-backend-8wqc.onrender.com",
     },
   },
 });
